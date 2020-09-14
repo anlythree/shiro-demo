21c8a900888809d5f9810895f01bd24cb5e384b7
wangli <anlythree@gmail.com>
2020-09-14 15:07:59 +0800

    创建项目

---
+++ src/main/java/top/anly/ShiroDemoApplication.java
@@ -0,0 +1,15 @@
+package top.anly;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @author wangli
+ * @date 2020/9/14 14:59
+ */
+@SpringBootApplication
+@EnableRedisHttpSession
+public class ShiroDemoApplication {
+
+
+}
