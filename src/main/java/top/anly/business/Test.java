a746229adb7ef6b2f92879b1a7025b60a2fd8324
wangli <anlythree@gmail.com>
2021-04-23 17:21:04 +0800

    分支同步

--- src/main/java/top/anly/business/Test.java
+++
@@ -1,8 +0,0 @@
-package top.anly.business;
-
-/**
- * @author wangli
- * @date 2021/4/23 17:17
- */
-public class Test {
-}
---
+++ src/main/java/top/anly/common/Test.java
@@ -0,0 +1,8 @@
+package top.anly.common;
+
+/**
+ * @author wangli
+ * @date 2021/4/23 17:20
+ */
+public class Test {
+}
